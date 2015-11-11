4a03bfaa8fc089b6ce4135850f2aceb6801ae580
mkody91 <kmiller91@southhills.edu>
2015-11-11 13:46:30 -0500

    division event handler
    added event handlers on the subButton division Buttons

--- js/main.js
+++ js/main.js
@@ -1,43 +1,83 @@
 
 
-	
-	
+
+
 
 
 $(document).ready(function(){
-    $(function() {
-        var params = {
 
-        };
-      
-        $.ajax({
-            url: "https://api.fantasydata.net/nfl/v2/XML/Standings/2015?subscription-key=3c48652a252840d09ffa2abf78a2605d" + $.param(params),
-            beforeSend: function(xhrObj){
-                // Request headers
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","3c48652a252840d09ffa2abf78a2605d");
-            },
-            type: "GET",
-            // Request body
-            data: "{}",
-        })
-        .done(function(data) {
-            alert("success");
-        })
-        .fail(function() {
-            alert("error");
-        });
+		var jsonData;
+
+		$.getJSON("https://api.fantasydata.net/nfl/v2/JSON/Standings/2015?subscription-key=3c48652a252840d09ffa2abf78a2605d", function(data) {
+			alert(data[1].name);
+		});
+
+
+    $.ajax({
+        url: "https://api.fantasydata.net/nfl/v2/JSON/Standings/2015?subscription-key=3c48652a252840d09ffa2abf78a2605d",
+        type: "GET"
+    })
+    .done(function(data) {
+        alert("success");
+				jsonData = data;
+				alert(data[0].Name);
+
+    })
+    .fail(function(data) {
+        alert("error");
+				alert(data.Name);
     });
-	$(".subButton").hide();
-	$("#afc").click(function(){
-		$(".mainButton").hide();
-		$("#afc.subButton").show();
-	});
-	$("#nfc").click(function(){
-		$(".mainButton").hide();
-		$("#nfc.subButton").show();
-	});
-	$("#previous").click(function(){
+
+
+		//Starts by hiding the sub-buttons which are the individual divisions
 		$(".subButton").hide();
-		$(".mainButton").show();
-	});
+		//function for what happens when you click on the afc main button
+		$("#afcMain").click(function(){
+			$(".mainButton").hide();
+			$(".afc.subButton").show();
+		});
+
+		$("#nfcMain").click(function(){
+			$(".mainButton").hide();
+			$(".nfc.subButton").show();
+		});
+
+
+		$("#afcNorth").click(function(){
+			$(".subButton").hide();
+		});
+		$("#afcEast").click(function(){
+			$(".subButton").hide();
+		});
+		$("#afcSouth").click(function(){
+			$(".subButton").hide();
+		});
+		$("#afcWest").click(function(){
+			$(".subButton").hide();
+		});
+		$("#nfcNorth").click(function(){
+			$(".subButton").hide();
+		});
+		$("#nfcEast").click(function(){
+			$(".subButton").hide();
+		});
+		$("#nfcSouth").click(function(){
+			$(".subButton").hide();
+		});
+		$("#nfcWest").click(function(){
+			$(".subButton").hide();
+		});
+		$("#previous").click(function(){
+			if($(".subButton:hidden" && ".mainButton:hidden")){
+				$(".subButton").show();
+				$(".mainButton").hide();
+			}
+			if($(".mainButton:hidden" && "subButton:visible")){
+				$(".mainButton").show();
+				$(".subButton").hide();
+			}
+		});
+
+
+
 });
